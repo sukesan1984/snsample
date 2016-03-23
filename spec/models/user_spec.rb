595ecb10192912c192039521e750eca07fdbf8de
sukesan1984 <takamikousuke@gmail.com>
2016-03-23 19:20:06 +0900

    add test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid with last_name over 20 lengths'  do
+    user = User.new(
+      last_name: 'abcdefghijklmnopqrstuxyiw',
+      first_name: 'kosuke',
+      email: 'test@test.com',
+      password: 'password'
+    )
+    user.valid?
+    expect(user.errors[:last_name]).to include("is too long (maximum is 20 characters)")
+  end
 end
