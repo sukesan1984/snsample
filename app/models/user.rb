8afb6d3dfcfbcc693a206b8a2d40bd007435abed
sukesan1984 <takamikousuke@gmail.com>
2016-03-23 19:00:53 +0900

    氏名の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :last_name, presence: true, length: {maximum: 20}
+  validates :first_name, presence: true, length: {maximum: 20}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323081946) do
+ActiveRecord::Schema.define(version: 20160323085227) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160323081946) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "last_name",              limit: 255,              null: false
+    t.string   "first_name",             limit: 255,              null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
