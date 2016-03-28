81686ce5707a5bfd63f83f1d29ef399701441fe9
sukesan1984 <takamikousuke@gmail.com>
2016-03-28 15:43:58 +0900

    add table relationships

---
+++ app/models/relationship.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer          not null, primary key
+#  follower_id :integer          not null
+#  followed_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_relationships_on_followed_id                  (followed_id)
+#  index_relationships_on_follower_id                  (follower_id)
+#  index_relationships_on_follower_id_and_followed_id  (follower_id,followed_id) UNIQUE
+#
+
+class Relationship < ActiveRecord::Base
+end
