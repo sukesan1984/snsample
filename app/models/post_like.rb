fd3f4ad13aab9989d2c4835a7f894cdba661f44e
sukesan1984 <takamikousuke@gmail.com>
2016-03-30 14:12:11 +0900

    add model post_like

---
+++ app/models/post_like.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: post_likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  post_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class PostLike < ActiveRecord::Base
+end
